03dc5fecba779e0b3b5465601baf1f2c112228f7
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-03-20 22:04:27 +0530

    first commit

---
+++ src/components/Fotter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Fotter() {
+    return (
+        <div className='flex justify-center items-center flex-col text-black-300 mb-6 gap-2'>
+            <div>
+                ----- Made By<a className='pl-1 text-blue-700 font-medium' href="https://twitter.com/HarshitD_9h7d9x">HarshitD</a><a className='px-1 text-blue-700 font-medium' >Finance Dashboard UI</a>-----
+            </div>
+            <div  className='text-black-300 underline' ><a href="https://github.com/9H7D9x/Dukan_Dashboard">Get the source code</a></div>
+        </div>
+    )
+}
+
+export default Fotter
